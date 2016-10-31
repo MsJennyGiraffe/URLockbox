360ee6f264636131b34a42160d8df87e6218850e
Jennifer <jennifersoden@hotmail.com>
2016-10-31 12:23:53 -0600

    Add links and gem for validation of urls

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031151522) do
+ActiveRecord::Schema.define(version: 20161031182301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string "url"
+    t.string "title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
