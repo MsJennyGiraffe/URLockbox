ffe90ed31de8aadcf6e14945a7429ed4e3819bb2
Jennifer <jennifersoden@hotmail.com>
2016-10-31 12:27:39 -0600

    Add read to links

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031182301) do
+ActiveRecord::Schema.define(version: 20161031182431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "links", force: :cascade do |t|
-    t.string "url"
-    t.string "title"
+    t.string  "url"
+    t.string  "title"
+    t.boolean "read",  default: false
   end
 
   create_table "users", force: :cascade do |t|
