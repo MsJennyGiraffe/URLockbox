4a02e4240a4f85ff4f7de456f0fe8b562042cf82
Jennifer <jennifersoden@hotmail.com>
2016-10-31 12:09:50 -0600

    Add test for user can view links
    A logged in user can see links
    A non logged in user is prompted to sign in to view content

---
+++ spec/features/logged_in_user_can_view_links_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "view links" do
+  scenario "a registered user can view links" do
+    visit "/"
+    click_link "Sign Up"
+    fill_in :user_email, with: "validuser@email.com"
+    fill_in :user_password, with: "password"
+    fill_in :user_password_confirmation, with: "password"
+    click_button "Submit"
+
+    expect(current_path).to eq(links_path)
+    expect(page).to_not have_content("Sign in to View Links")
+  end
+  
+  scenario "a nonlogged in user cannot view links" do
+    visit "/links"
+
+    expect(page).to have_content("Sign in to View Links")
+  end
+end
