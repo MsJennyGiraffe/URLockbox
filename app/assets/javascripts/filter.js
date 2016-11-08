df1aad5f84ffa3e435bdbb98cbb95409f78284c4
Jennifer <jennifersoden@hotmail.com>
2016-11-08 13:49:12 -0700

    Filter doesn't work

---
+++ app/assets/javascripts/filter.js
@@ -0,0 +1,36 @@
+var responseArray = [];
+
+function addEventListenerToTextField() {
+  $("#text-field").on("keydown", function(event) {
+    responseArray.push(event.key);
+    searchUrls();
+  })
+}
+
+// function searchUrls() {
+//   // if class contains link or if class is title
+//   var titleClass = $(".title");
+//   var titleArray = []
+//   for (i = 0; i < titleClass.length; i++) {
+//     titleArray.push(titleClass[i].innerHTML.split("Title: ")[1])
+//   }
+//
+//   titleArray.forEach(function(title) {
+//     if (title.match(responseArray.join(""))) {
+//
+//
+//     }
+//   })
+//
+// }
+
+function searchUrls() {
+  searchTerm = responseArray.join("")
+  var links = $(".url");
+  for (i = 0; i < links.length; i++) {
+  var url = $(links[i]).context
+    // .match(searchTerm)
+    console.log(links[i])
+  }
+
+}
