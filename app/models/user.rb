867964c36fdeb25ee071eba616c8516dec38df3e
Jennifer <jennifersoden@hotmail.com>
2016-11-08 10:04:35 -0700

    Password confirmation must match now

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :links
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, confirmation: true
 end
