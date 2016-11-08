4bec77b2300d50cfb4e41c7fbea27db10551b4e2
Jennifer <jennifersoden@hotmail.com>
2016-11-08 10:55:49 -0700

    Can sort

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -19,6 +19,7 @@ function sortCall(sortType) {
 }
 
 function renderResponse(response) {
+  $(".urls").empty();
   for (i = 0; i < response.length; i++) {
     appendLinkToPage(response[i])
   }
@@ -29,5 +30,50 @@ function appendLinkToPage(link) {
 }
 
 function createLinkHTML(link) {
-  return link.title + link.url
+  return (
+    "<div class='col-md-4'>" +
+    "<br>" +
+    "<div class='url-" +
+    link.id +
+    "'>" +
+    "<h5> Title: " +
+    link.title +
+    "</h5>" +
+    isRead(link) +
+    "<h5>" +
+    "<a href='links/" +
+    link.id +
+    "/edit' class='btn btn-info btn-block'>" +
+    "Edit Link" +
+    "</a>" +
+    "</div>" +
+    "</div>"
+  )
+}
+
+function isRead (link) {
+  if (link.read) {
+    return (
+      "<h5 class='link-" +
+      link.id +
+      "strike'>URL:" +
+      link.url +
+      "</h5>" +
+      "<button id='" +
+      link.id +
+      "-read-button' class='unread-button btn btn-info btn-block'>Mark as Unread</button>"
+    )
+  }
+  else {
+     return (
+       "<h5 class='link-" +
+        link.id +
+        "'>URL: " +
+        link.url +
+        "</h5>" +
+        "<button id='" +
+        link.id +
+        "-read-button' class='read-button btn btn-info btn-block'>Mark as Read</button>"
+     )
+  }
 }
